0a989eb901f6a11347683ac75bcdd290420b37f7
Gleb <gleb.zhukel@gmail.com>
2018-07-18 00:50:55 +0300

    first

---
+++ src/main/java/com/qtex/api/QtexApplication.java
@@ -0,0 +1,12 @@
+package com.qtex.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QtexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QtexApplication.class, args);
+	}
+}
